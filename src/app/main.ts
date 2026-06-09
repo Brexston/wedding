b9ea51f90abe72635dabcd4158f0bfae47fe735a
Yuriy Sokolov <79514840030@ya.ru>
2026-06-09 21:09:52 +0500

    feat: накидали блоков

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,6 @@
 /* eslint-disable perfectionist/sort-imports */
 
 // Import styles
-import '@fontsource/bad-script/400.css';
 import '@fontsource/montserrat/400.css';
 import '@fontsource/montserrat/500.css';
 import '@styles/index.sass';
